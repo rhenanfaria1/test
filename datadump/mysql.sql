21a31ce09064d36341a146637885ea28a899d1ef
Rhenan Faria <115506977+rhenanfaria1@users.noreply.github.com>
2025-01-13 10:35:15 -0300

    Update mysql.sql

--- datadump/mysql.sql
+++ datadump/mysql.sql
@@ -1,3 +1,4 @@
 CREATE TABLE test.messages (message VARCHAR(250));
-INSERT INTO test.messages VALUES ('hello');
-INSERT INTO test.messages VALUES ('hey');
+INSERT INTO test.messages VALUES ('Relatório clientes ativos');
+INSERT INTO test.messages VALUES ('Relatório oportunidades abertas');
+INSERT INTO test.messages VALUES ('Relatório Treinamentos ativos por clientes');
