0401ec50c7582bba9bac50b7110d47a3a7ad398a
Rhenan Faria <115506977+rhenanfaria1@users.noreply.github.com>
2025-01-13 13:53:52 -0300

    Update clientes.sql

--- datadump/clientes.sql
+++ datadump/clientes.sql
@@ -1,4 +1,3 @@
-CREATE TABLE test.messages (message VARCHAR(250));
 INSERT INTO test.messages VALUES ('Relatório clientes ativos');
 INSERT INTO test.messages VALUES ('Relatório oportunidades abertas');
 INSERT INTO test.messages VALUES ('Relatório Treinamentos ativos por clientes');
