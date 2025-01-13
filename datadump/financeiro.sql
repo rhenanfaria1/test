9c73a78331d676749c7f55118b52783aeaa90aa6
Rhenan Faria <115506977+rhenanfaria1@users.noreply.github.com>
2025-01-13 14:03:53 -0300

    Update and rename financas.sql to financeiro.sql

--- datadump/financeiro.sql
+++ datadump/financeiro.sql
@@ -1,3 +1,4 @@
+CREATE TABLE test.messages (message VARCHAR(250));
 INSERT INTO test.messages VALUES ('Relatorio Finanças');
 INSERT INTO test.messages VALUES ('Relatório Folha de Pagamento');
 INSERT INTO test.messages VALUES ('Relatório Folha de Comissão');
