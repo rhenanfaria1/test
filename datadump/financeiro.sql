794194cce29e877fe8b287d42a68a28291031d4e
Rhenan Faria <115506977+rhenanfaria1@users.noreply.github.com>
2025-01-14 16:54:44 -0300

    Update financeiro.sql

--- datadump/financeiro.sql
+++ datadump/financeiro.sql
@@ -1,3 +1,3 @@
 INSERT INTO test.messages VALUES ('Folha de Pagamento');
-INSERT INTO test.messages VALUES ('Comissões');
+INSERT INTO test.messages VALUES ('Comissoes');
 INSERT INTO test.messages VALUES ('Extrato Financeiro');
